15b1ac99ac640975d1dd6d11f9414477f4d52047
maryuriruiz <maryuriruizv08@gmail.com>
2026-09-18 21:52:12 -0500

    Creo mi constructor

---
+++ src/Personaje.java
@@ -0,0 +1,17 @@
+//Clase padre
+
+public class Personaje {
+    private int id;
+    private String nombre;
+    private int nivel;
+
+    //Constructor
+    public Personaje(int id, String nombre, int nivel){
+        this.id = id;
+        this.String = nombre;
+        this.int = nivel;
+    }
+
+    
+
+}
