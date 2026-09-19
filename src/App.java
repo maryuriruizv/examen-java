7ce49612f21d7dca860d3a0c8a88d02c46784956
maryuriruiz <maryuriruizv08@gmail.com>
2026-09-18 21:41:29 -0500

    Inicio mi examen Java

---
+++ src/App.java
@@ -0,0 +1,20 @@
+/*
+EXAMEN FINAL - JAVA POO
+
+Autor: Maryuri Ruiz 
+Fecha: 18 de septiembre de 2026
+
+Enunciado: 
+    Una academia de videojuegos necesita un programa para registrar personajes. Todos comparten un
+    identificador, un nombre y un nivel, pero cada tipo realiza una acción diferente. El sistema debe guardar los
+    objetos en una sola colección, mostrarlos y buscar uno por su identificador.
+*/
+
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
